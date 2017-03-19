ab461dd92c658d2d26a72892f7f040acb6f05e61
DaiTianyu <dtysky@outlook.com>
2017-03-19 17:48:20 +0800

    Complete algorithm: WeightedDigraphSPDijkstra.

--- Algorithm/weighted_digraph_algorithm.h
+++ Algorithm/weighted_digraph_algorithm.h
@@ -24,7 +24,122 @@ namespace my_algorithm {
     using data_structures::SymbolTable;
     using data_structures::PriorityQueue;
 
-
+    // only works on connected graph and the weight must be positive
+    template <typename T, typename Weight>
+    class WeightedDigraphSPDijkstra {
+        typedef RBTreeNode<TreeElement<T, WeightedGraphAdjSet<T, Weight>>> Node;
+        typedef WeightedGraphEdge<Node, Weight> Edge;
+        struct SPElement {
+        public:
+            T vertex;
+            Weight weight;
+            SPElement() {}
+            SPElement(T vertex, const Weight& weight) {
+                this->vertex = vertex;
+                this->weight = weight;
+            }
+            SPElement(const SPElement& element) {
+                vertex = element.vertex;
+                weight = element.weight;
+            }
+            SPElement& operator=(const SPElement& element) {
+                vertex = element.vertex;
+                weight = element.weight;
+                return *this;
+            }
+            bool operator==(const SPElement& element) const {
+                return vertex == element.vertex;
+            }
+            bool operator!=(const SPElement& element) const {
+                return vertex != element.vertex;
+            }
+            bool operator>(const SPElement& element) const {
+                return weight > element.weight;
+            }
+            bool operator<(const SPElement& element) const {
+                return weight < element.weight;
+            }
+            bool operator>=(const SPElement& element) const {
+                return weight >= element.weight;
+            }
+            bool operator<=(const SPElement& element) const {
+                return weight <= element.weight;
+            }
+        };
+    protected:
+        T _start;
+        SymbolTable<T, Edge> _edge_to;
+        SymbolTable<T, Weight> _weight_to;
+        PriorityQueue<SPElement> _queue;
+        void _relax(WeightedDigraph<T, Weight> &graph, const SPElement& element) {
+            auto vertex = element.vertex;
+            auto weight = element.weight;
+            auto node = graph.getNode(vertex);
+            auto edges = node->element.value.adjEdges();
+            for (auto &edge: edges) {
+                auto to_vertex = edge.to->element.key;
+                auto new_weight = weight + edge.weight;
+                if (!_weight_to.has(to_vertex)) {
+                    _weight_to.set(to_vertex, new_weight);
+                    _edge_to.set(to_vertex, edge);
+                    _queue.enqueue(SPElement(to_vertex, new_weight));
+                    continue;
+                }
+                if (_weight_to.get(to_vertex) > new_weight) {
+                    _weight_to.set(to_vertex, new_weight);
+                    _edge_to.set(to_vertex, edge);
+                    // todo: create a new priority queue which supports function `change`
+                    auto size = _queue.size();
+                    for (size_t i = 1; i <= size; i++) {
+                        if (_queue.minN(i).vertex == to_vertex) {
+                            _queue.dequeueMinN(i);
+                            break;
+                        }
+                    }
+                    _queue.enqueue(SPElement(to_vertex, new_weight));
+                }
+            }
+        }
+    public:
+        WeightedDigraphSPDijkstra(WeightedDigraph<T, Weight> &graph, T vertex) {
+            _start = vertex;
+            _edge_to.clear();
+            _weight_to.clear();
+            _queue.clear();
+            auto start = graph.getNode(vertex);
+            _edge_to.set(vertex, Edge(start, start, Weight()));
+            _weight_to.set(vertex, Weight());
+            _queue.enqueue(SPElement(_start, Weight()));
+            while (!_queue.isEmpty()) {
+                _relax(graph, _queue.dequeueMinN(1));
+            }
+        }
+        virtual ~WeightedDigraphSPDijkstra() {}
+        Weight weightTo(T vertex) {
+            if (!hasPathTo(vertex)) {
+                throw std::out_of_range("No path!");
+            }
+            return _weight_to.get(vertex);
+        }
+        bool hasPathTo(T vertex) {
+            return _weight_to.has(vertex);
+        }
+        std::vector<Edge>pathTo(T vertex) {
+            if (!hasPathTo(vertex)) {
+                throw std::out_of_range("No path!");
+            }
+            std::vector<Edge> path;
+            auto edge = _edge_to.get(vertex);
+            while (true) {
+                path.insert(path.begin(), edge);
+                if (edge.from->element.key == _start) {
+                    break;
+                }
+                edge = _edge_to.get(edge.from->element.key);
+            }
+            return path;
+        }
+    };
 }
 
 #endif //ALGORITHM_WEIGHTED_DIGRAPH_ALGORITHM_H
---
+++ Tests/weighted_digraph_algorithm.test.h
@@ -0,0 +1,65 @@
+//
+//  weighted_digraph_algorithm
+//  Algorithm
+//
+//  Created by dtysky on 2017/3/19.
+//  Copyright © 2017 dtysky@outlook.com. All rights reserved.
+//
+
+#ifndef ALGORITHM_WEIGHTED_DIGRAPH_ALGORITHM_TEST_H
+#define ALGORITHM_WEIGHTED_DIGRAPH_ALGORITHM_TEST_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include "weighted_digraph.h"
+#include "weighted_digraph_algorithm.h"
+
+
+namespace my_algorithm {
+    using std::cout;
+    using std::endl;
+    using data_structures::WeightedDigraph;
+
+    void testWeightDigraphAlgorithm() {
+        WeightedDigraph<int, int> graph;
+        for (int i = 0; i <= 7; i++) {
+            graph.addVertex(i);
+        }
+
+        graph.addEdge(4, 5, 35)
+            .addEdge(5, 4, 35)
+            .addEdge(4, 7, 37)
+            .addEdge(5, 7, 28)
+            .addEdge(7, 5, 28)
+            .addEdge(5, 1, 32)
+            .addEdge(0, 4, 38)
+            .addEdge(0, 2, 26)
+            .addEdge(7, 3, 39)
+            .addEdge(1, 3, 29)
+            .addEdge(2, 7, 34)
+            .addEdge(6, 2, 40)
+            .addEdge(3, 6, 52)
+            .addEdge(6, 0, 58)
+            .addEdge(6, 4, 93);
+
+        cout << graph << endl;
+
+        cout << "<---- WeightedDigraphSPDijkstra ---->" << endl;
+        auto a_sp_dij = WeightedDigraphSPDijkstra<int, int>(graph, 0);
+        for (int i = 0; i <= 7; i++) {
+            if (!a_sp_dij.hasPathTo(i)) {
+                continue;
+            }
+            cout << 0 << " to " << i << ":" << endl;
+            cout << "weight: " << a_sp_dij.weightTo(i) << endl;
+            cout << "path: ";
+            for (auto &edge: a_sp_dij.pathTo(i)) {
+                cout << edge << " ";
+            }
+            cout << endl;
+        }
+    }
+}
+
+#endif //ALGORITHM_WEIGHTED_DIGRAPH_ALGORITHM_TEST_H
