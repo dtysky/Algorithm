da865b48ad56f94e1c308e64903e2877551f1641
DaiTianyu <dtysky@outlook.com>
2017-03-26 00:49:59 +0800

    Complete datastructure: Alphabet.

---
+++ String/alphabet.rb
@@ -0,0 +1,55 @@
+class Alphabet
+  def initialize str
+    @table = []
+    str.split('').each do |char|
+      if not @table.include? char
+        @table.push char
+      end
+    end
+    @radix = @table.length
+  end
+
+  def to_char index
+    @table[index]
+  end
+
+  def to_index char
+    @table.index char
+  end
+
+  def contains char
+    @table.include? char
+  end
+
+  def radix
+    @radix
+  end
+
+  def lg_radix
+    (Math.sqrt @radix).to_int + 1
+  end
+
+  def to_indices str
+    str.split('').map do |char|
+      if @table.include? char
+        @table.index char
+      else
+        raise "This alphabet does not has char #{char} !"
+      end
+    end
+  end
+
+  def to_chars indices
+    indices.map do |index|
+      if @radix > index
+        @table[index]
+      else
+        raise "This alphabet only has #{@radix} characters !"
+      end
+    end
+  end
+
+  def to_s
+    @table.to_s
+  end
+end
---
+++ String/alphabet_test.rb
@@ -0,0 +1,56 @@
+require 'test/unit'
+require './alphabet'
+
+class MyTest < Test::Unit::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    @ab = Alphabet.new 'dtysky'
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+  def test_constructor
+    assert @ab.to_s == '["d", "t", "y", "s", "k"]'
+    puts @ab.to_s
+  end
+
+  def test_radix
+    assert @ab.radix == 5
+    assert @ab.lg_radix == 3
+    print @ab.radix, " ", @ab.lg_radix
+    puts
+  end
+
+  def test_to_char_and_to_index
+    @ab.radix.times do |index|
+      char = @ab.to_char index
+      print char, " ", @ab.to_index(char)
+      puts
+    end
+  end
+
+  def test_to_indices
+    p @ab.to_indices 'dtysky'
+    begin
+      @ab.to_indices '1'
+    rescue => err
+      puts err.message
+    end
+  end
+
+  def test_to_chars
+    p @ab.to_chars [0, 1, 2, 3, 4]
+    begin
+      @ab.to_chars [5]
+    rescue => err
+      puts err.message
+    end
+  end
+end
